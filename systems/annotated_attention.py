4e23fa39442038741e5d2c984561ae40ffae5a0e
Surya-Prasad <suryaramana99@gmail.com>
2026-05-01 03:29:51 -0400

    nSight Ready Script

---
+++ systems/annotated_attention.py
@@ -0,0 +1,21 @@
+import math
+import torch
+import torch.cuda.nvtx as nvtx
+from basics.basics.nn_utils import softmax 
+
+@nvtx.range("scaled dot product attention")
+def annotated_scaled_dot_product_attention(Q, K, V, mask=None):
+    d_k = K.shape[-1]
+    
+    with nvtx.range("computing attention scores"):
+        attention_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores = torch.where(mask, attention_scores, float("-inf"))
+
+    with nvtx.range("computing softmax"):
+        attention_weights = softmax(attention_scores, dim=-1)
+
+    with nvtx.range("final matmul"):
+        output = torch.matmul(attention_weights, V)
+
+    return output
--- systems/performance-evaluator.py
+++ systems/performance-evaluator.py
@@ -2,7 +2,10 @@ import argparse
 import torch
 import numpy as np
 from basics.basics.model import BasicsTransformerLM
+import torch.cuda.nvtx as nvtx
 import timeit
+
+import contextlib
 
 def get_arguments(): 
     parser = argparse.ArgumentParser(description='Arguments for Performance Evaluator Script')
@@ -16,12 +19,25 @@ def get_arguments():
     parser.add_argument('--w', type = int, default = 5)
     parser.add_argument('--measure_steps', type = int, default = 10)
     parser.add_argument('--backward', action="store_true")
+    parser.add_argument("--optimizer", action="store_true")
+    parser.add_argument("--profile", action="store_true")
 
     return parser.parse_args()
 
 def main():
     args = get_arguments()
     device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+
+    if args.profile: 
+        try: 
+            import basics.basics.model
+            from systems.annotated_attention import annotated_scaled_dot_product_attention
+
+            basics.basics.model.scaled_dot_product_attention = annotated_scaled_dot_product_attention
+            print("Using annotated attention for profiling.")
+        except ImportError:
+            print(f"Could not import annotated attention: {ImportError}")
+
     # Model init
     model = BasicsTransformerLM(
         vocab_size=args.vocab_size,
@@ -43,44 +59,69 @@ def main():
     # Backward pass loss function
     loss_fn = torch.nn.CrossEntropyLoss()
     dummy = torch.randint(0, args.vocab_size, (args.batch_size, args.context_length), device=device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
 
-    def one_step(): 
-        # forward
-        logits = model(x)
+    # Get range for nvtx
+    def get_nvtx_range(name): 
+        if args.profile and device.type == "cuda":
+            return torch.cuda.nvtx.range(name)
+        return contextlib.nullcontext()
 
-        if args.backward:
-            loss = loss_fn(logits.view(-1, args.vocab_size), dummy.view(-1))
-            loss.backward()
+    def one_step(phase_name = 'measurement'): 
+        if args.optimizer: 
+            optimizer.zero_grad()
 
-    # Run one step
-    for _ in range(args.w):
-        one_step()
-        if device.type == 'cuda':
-            torch.cuda.synchronize()
+        with get_nvtx_range(f"{phase_name}-forward-pass"):
+            # forward
+            logits = model(x)
+
+        if args.backward or args.optimizer:
+            with get_nvtx_range(f"{phase_name}-loss-or-backward-pass"):
+                loss = loss_fn(logits.view(-1, args.vocab_size), dummy.view(-1))
+                loss.backward()
+
+        if args.optimizer:
+            with get_nvtx_range(f"{phase_name}-optimizer-step"):
+                optimizer.step()
+
+    # Warmup Phase
+    with get_nvtx_range("warmup-phase"):
+        for _ in range(args.w):
+            one_step(phase_name="warmup")
+            if device.type == 'cuda':
+                torch.cuda.synchronize()
         
     # Measuring Performance
     lap_times = []
 
-    for _ in range(args.measure_steps):
-        if device.type == 'cuda':
-            torch.cuda.synchronize()
+    with get_nvtx_range("measurement-phase"):
+        for _ in range(args.measure_steps):
+            if device.type == 'cuda':
+                torch.cuda.synchronize()
 
-        start_time = timeit.default_timer()
-        one_step()
+            start_time = timeit.default_timer()
+            one_step(phase_name='measurement')
 
-        if device.type == 'cuda':
-            torch.cuda.synchronize()
+            if device.type == 'cuda':
+                torch.cuda.synchronize()
         
-        # one lap is end - start
-        lap_times.append(timeit.default_timer() - start_time)
+            # one lap is end - start
+            lap_times.append(timeit.default_timer() - start_time)
 
     mean_time = np.mean(lap_times)
     std_time = np.std(lap_times)
 
-    mode = "Forward + backward" if args.backward else "Only Forward"
+    # mode = "Forward + backward" if args.backward else "Only Forward"
+    if args.optimizer: 
+        mode = "forward+backward+optimizer step"
+    elif args.backward: 
+        mode = "forward+backward"
+    else: 
+        mode = "only-forward"
+
     print(f"Mode of Running: {mode}")
-    print(f"Mean time: {mean_time} seconds")
-    print(f"Std time: {std_time} seconds")
+    print(f"Mean time: {mean_time:.4f} seconds")
+    print(f"Std time: {std_time:.4f} seconds")
 
 
 if __name__ == "__main__":
